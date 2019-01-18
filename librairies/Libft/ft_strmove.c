a15e8209901e442c27f0effaf919682612ed7f44
Enzo POGGIO <poggio.enzo@gmail.com>
2019-01-18 20:57:03 +0100

    update_to_compile_libft

--- librairies/Libft/ft_strmove.c
+++ librairies/Libft/ft_strmove.c
@@ -3,10 +3,10 @@
 /*                                                              /             */
 /*   ft_strmove.c                                     .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: simrossi <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*   By: simrossi <simrossi@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/19 07:05:54 by simrossi     #+#   ##    ##    #+#       */
-/*   Updated: 2019/01/15 15:01:12 by simrossi    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/01/18 20:13:03 by epoggio     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -22,9 +22,11 @@
 
 void	ft_strmove(char *old_start, char *new_begin)
 {
-	size_t	new_len;
+//	size_t	new_len;
+	old_start++; 
+	new_begin++;
+// 	new_len = ft_strlen(new_begin);
+// 	ft_memmove(old_start, new_begin, new_len);
+// 	ft_bzero(old_start + new_len, new_begin - begin_str);
 
-	new_len = ft_strlen(new_begin);
-	ft_memmove(old_start, new_begin, new_len);
-	ft_bzero(old_start + new_len, new_begin - begin_str);
 }
