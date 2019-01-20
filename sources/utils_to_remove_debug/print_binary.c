f7209c7007b7a7dfce7d35e124c8dcc94376a1e0
Enzo POGGIO <poggio.enzo@gmail.com>
2019-01-20 19:14:42 +0100

    printf -> ft_printf

--- sources/utils_to_remove_debug/print_binary.c
+++ sources/utils_to_remove_debug/print_binary.c
@@ -1,14 +1,15 @@
 #include "utils.h"
+#include "libft.h"
 
 void print_binary(long nb , int iter)
 {
 	if (iter)
 		print_binary(nb>>1 ,--iter);
-	printf("%d", nb % 2 != 0);
+	ft_printf("%d", nb % 2 != 0);
 }
 
 void print_short(short to_print)
 {
 	print_binary((long)to_print, 11);
-	printf("\n");
+	ft_printf("\n");
 }
