3765ae408ca8b4fd7132f94d00111b996a3e873f
Enzo POGGIO <poggio.enzo@gmail.com>
2019-01-20 21:32:44 +0100

    rename absolute_path -> relative_apth free only malloc string set_null_tfile get value for each attributes

--- sources/tfile_utils.c
+++ sources/tfile_utils.c
@@ -1,6 +1,6 @@
 #include "ft_ls.h"
 
-char		*absolute_path(char *folder, char *file)
+char		*relative_path(char *folder, char *file)
 {
 	char	*slash;
 	int		allocate;
@@ -27,9 +27,9 @@ char		*absolute_path(char *folder, char *file)
 int		free_file_node(t_file **folder, int status)
 {
 	free((*folder)->path);
-	free((*folder)->pw_name);
-	free((*folder)->modification_time);
-	free((*folder)->symbolic_link);
+	//free((*folder)->symbolic_link);
+	free((*folder)->filename);
+	free((*folder)->info);
 	free(*folder);
 	*folder = NULL;
 	return (status);
@@ -48,12 +48,24 @@ int		free_folder(t_file **folder, int status)
 	return (free_file_node(folder, status));
 }
 
+static void set_null_tfile(t_file **node)
+{
+	(*node)->info = NULL;
+	(*node)->path = NULL;
+	(*node)->filename = NULL;
+	(*node)->pw_name = NULL;
+	(*node)->gr_name = NULL;
+	(*node)->modification_time = NULL;
+	//(*node)->symbolic_link = NULL;
+	(*node)->next = NULL;
+}
+
 /*
 ** create_file:
 **
 ** Allocate a new t_file element, set almost all attribute to NULL,
 ** and malloc also the struct stat *info element.
-** 
+**
 ** return value:
 ** - -1 if an allocation error occur.
 ** - 1 if the allocation succeed.
@@ -66,18 +78,19 @@ int		create_tfile(char *parent, char *path, t_file **node)
 	error = 1;
 	if (!(*node = (t_file *)malloc(sizeof(t_file))))
 		return (-1);
-	(*node)->path = NULL;
-	(*node)->pw_name = NULL;
-	(*node)->gr_name = NULL;
-	(*node)->modification_time = NULL;
-	(*node)->symbolic_link = NULL;
-	(*node)->next = NULL;
-	(*node)->filename = NULL;
-	error *= ((*node)->path = absolute_path(parent, path)) != NULL ? 1 : 0;
+	set_null_tfile(node);
+	error *= ((*node)->path = relative_path(parent, path)) != NULL ? 1 : 0;
+
 	error *= ((*node)->filename = ft_strdup(path)) != NULL ? 1 : 0;
 	if (!((*node)->info = (struct stat*)malloc(sizeof(struct stat))))
 		return (free_file_node(node, -1));
 	error *= lstat((*node)->path, (*node)->info) == 0 ? 1 : 0;
+	(*node)->pw_name = (getpwuid((*node)->info->st_uid))->pw_name;
+	(*node)->gr_name = (getgrgid((*node)->info->st_gid))->gr_name;
+	(*node)->modification_time = ctime(&(*node)->info->st_mtimespec.tv_sec);
+	(*node)->symbolic_link[0] = 0;
+	if (S_ISLNK((*node)->info->st_mode))
+		readlink((*node)->path, (*node)->symbolic_link, PATH_MAX);
 	return (error == 1 ? 1: free_file_node(node, -1));
 }
 
